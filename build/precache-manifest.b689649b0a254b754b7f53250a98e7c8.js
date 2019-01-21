77b85c989c71c25654bdb84afb2c7e498b1c64f3
Vanutappi <44833114+Vanutappi@users.noreply.github.com>
2019-01-21 02:56:44 +0200

    Add files via upload

---
+++ build/precache-manifest.b689649b0a254b754b7f53250a98e7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32f4c8d85cb403ac00b4",
+    "url": "/static/js/main.32f4c8d8.chunk.js"
+  },
+  {
+    "revision": "a1dc948e747b96ba91fa",
+    "url": "/static/js/1.a1dc948e.chunk.js"
+  },
+  {
+    "revision": "32f4c8d85cb403ac00b4",
+    "url": "/static/css/main.21dc29dc.chunk.css"
+  },
+  {
+    "revision": "a1dc948e747b96ba91fa",
+    "url": "/static/css/1.e22797ac.chunk.css"
+  },
+  {
+    "revision": "90464c0e20d197439f00820624d06f66",
+    "url": "/index.html"
+  }
+];
